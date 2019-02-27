0de5b1b5c5867237f8e72d095f60a59dd754481b
KaoImin <652852910@qq.com>
2019-02-27 11:42:04 +0800

    add integration tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -8,6 +8,57 @@ use bft::*;
 use crossbeam::crossbeam_channel::{unbounded, Receiver, Sender};
 use rand::{thread_rng, Rng};
 
+const INIT_HEIGHT: usize = 1;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct Node {
+    height: usize,
+    result: Vec<(usize, Target)>,
+}
+
+impl Node {
+    fn new() -> Self {
+        Node {
+            height: INIT_HEIGHT,
+            result: Vec::new(),
+        }
+    }
+
+    fn handle_message(
+        &mut self,
+        msg: BftMsg,
+        s_1: Sender<BftMsg>,
+        s_2: Sender<BftMsg>,
+        s_3: Sender<BftMsg>,
+        s_result: Sender<Commit>,
+    ) {
+        match msg {
+            BftMsg::Proposal(proposal) => {
+                println!(
+                    "Node {:?} proposal {:?}",
+                    proposal.proposer, proposal.content
+                );
+                transmit_msg(BftMsg::Proposal(proposal), s_1, s_2, s_3);
+            }
+            BftMsg::Vote(vote) => {
+                println!(
+                    "Node {:?}, height {:?}, round {:?}, {:?} vote {:?}",
+                    vote.voter, vote.height, vote.round, vote.vote_type, vote.proposal
+                );
+                transmit_msg(BftMsg::Vote(vote), s_1, s_2, s_3);
+            }
+            BftMsg::Commit(commit) => {
+                self.result
+                    .push((commit.clone().height, commit.clone().proposal));
+                self.height = commit.height;
+                s_result.send(commit.clone()).unwrap();
+                self.height += 1;
+            }
+            _ => println!("Invalid Message Type!"),
+        }
+    }
+}
+
 fn start_process(address: Address) -> (Sender<BftMsg>, Receiver<BftMsg>) {
     let (main2bft, bft4main) = unbounded();
     let (bft2main, main4bft) = unbounded();
