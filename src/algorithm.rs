3f20598e41ecc978e7210e613ba2c123f7203988
rev-chaos <wancencen@cryptape.com>
2019-05-15 16:02:28 +0800

    generalize WaitTime

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -8,7 +8,7 @@ use crate::{
     wal::Wal,
 };
 
-use crossbeam::crossbeam_channel::{unbounded, Receiver, RecvError, Sender, select};
+use crossbeam::crossbeam_channel::{select, unbounded, Receiver, RecvError, Sender};
 use log::{debug, error, info, trace};
 use std::collections::HashMap;
 use std::sync::Arc;
--- src/timer.rs
+++ src/timer.rs
@@ -25,13 +25,13 @@ pub(crate) struct TimeoutInfo {
 }
 
 impl PartialOrd for TimeoutInfo {
-    fn partial_cmp(&self, other: &TimeoutInfo) -> Option<Ordering> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         self.timestamp.partial_cmp(&other.timestamp)
     }
 }
 
 impl Ord for TimeoutInfo {
-    fn cmp(&self, other: &TimeoutInfo) -> Ordering {
+    fn cmp(&self, other: &Self) -> Ordering {
         self.timestamp.cmp(&other.timestamp)
     }
 }
@@ -69,15 +69,31 @@ impl Decodable for TimeoutInfo {
     }
 }
 
-/// Sender and receiver of a timeout infomation channel.
-pub(crate) struct WaitTimer {
-    timer_seter: Receiver<TimeoutInfo>,
-    timer_notify: Sender<TimeoutInfo>,
+impl GetInstant for TimeoutInfo {
+    fn get_instant(&self) -> Instant {
+        self.timestamp
+    }
 }
 
-impl WaitTimer {
+pub trait GetInstant {
+    fn get_instant(&self) -> Instant;
+}
+
+/// Sender and receiver of a timeout infomation channel.
+pub struct WaitTimer<T>
+where
+    T: Eq + PartialEq + Ord + PartialOrd + Clone + GetInstant,
+{
+    timer_seter: Receiver<T>,
+    timer_notify: Sender<T>,
+}
+
+impl<T> WaitTimer<T>
+where
+    T: Eq + PartialEq + Ord + PartialOrd + Clone + GetInstant,
+{
     /// A function to create a new timeout infomation channel.
-    pub(crate) fn new(ts: Sender<TimeoutInfo>, rs: Receiver<TimeoutInfo>) -> WaitTimer {
+    pub fn new(ts: Sender<T>, rs: Receiver<T>) -> WaitTimer<T> {
         WaitTimer {
             timer_notify: ts,
             timer_seter: rs,
@@ -85,13 +101,13 @@ impl WaitTimer {
     }
 
     /// A function to start a timer.
-    pub(crate) fn start(&self) {
-        let mut timer_heap = MinMaxHeap::<TimeoutInfo>::new();
+    pub fn start(&self) {
+        let mut timer_heap = MinMaxHeap::<T>::new();
 
         loop {
             // take the peek of the min-heap-timer sub now as the sleep time otherwise set timeout as 100
             let timeout = if !timer_heap.is_empty() {
-                let peek_min_time = timer_heap.peek_min().unwrap().timestamp;
+                let peek_min_time = timer_heap.peek_min().unwrap().get_instant();
                 let now = Instant::now();
                 if peek_min_time > now {
                     peek_min_time - now
@@ -116,7 +132,7 @@ impl WaitTimer {
 
                 // if some timers are set as the same time, send timeout messages and pop them
                 while !timer_heap.is_empty()
-                    && now >= timer_heap.peek_min().cloned().unwrap().timestamp
+                    && now >= timer_heap.peek_min().cloned().unwrap().get_instant()
                 {
                     let time_info = timer_heap.pop_min().unwrap();
                     handle_err(self.timer_notify.send(time_info).map_err(|e| {
